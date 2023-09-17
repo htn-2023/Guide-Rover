6ce854e898290ac98eeb4c01da8eb51c49bc0a77
Tristan Jin <tjin368@gmail.com>
2023-09-17 07:29:13 -0400

    Last_night_of_grinding

--- RoboMaster/RoboMaster_Controller_YOLOv8_Classification.py
+++ RoboMaster/RoboMaster_Controller_YOLOv8_Classification.py
@@ -2,6 +2,8 @@ import cv2
 from ultralytics import YOLO
 import math
 from robomaster import robot
+from communication import decision
+import asyncio
 
 model = YOLO("./yolov8n.pt")
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
@@ -16,63 +18,137 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush", "hat"
               ]
 
-frame_width = 640
-frame_height = 640
+frame_width = 500
+frame_height = 500
 
 out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
 
 
-def camera():
-    img = ep_camera.read_cv2_image()
+def jsonToMovement(movement):
+    direction = movement['direction']
+    distance = movement['distance']
 
-    results = model(img, stream=True)
+    if not distance or not direction:
+        return 0, 0
 
-    for r in results:
-        boxes = r.boxes
-        for box in boxes:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            print(x1, y1, x2, y2)
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-            conf = math.ceil((box.conf[0] * 100)) / 100
-            cls = int(box.cls[0])
-            class_name = classNames[cls]
-            label = f'{class_name}{conf}'
-            t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
-            c2 = x1 + t_size[0], y1 - t_size[1] - 3
-            cv2.rectangle(img, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)  # filled
-            cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
-    out.write(img)
-    cv2.imshow("Robot", img)
-    cv2.waitKey(1)
+    radians = math.radians(movement['direction'])
 
+    x = math.cos(radians) / 10
+    y = math.sin(radians) / 10
+
+    return x, y
+
+
+async def waitOneSecond():
+    await asyncio.sleep(1)
+    return 0, 0
 
 if __name__ == '__main__':
-    ep_robot = robot.Robot()
-    ep_robot.initialize(conn_type="ap")
+    class RobotController:
+        def __init__(self):
+            self.ep_robot = robot.Robot()
+            self.ep_robot.initialize(conn_type="ap")
+            self.ep_chassis = self.ep_robot.chassis
+            self.ep_camera = self.ep_robot.camera
+            self.x_val = 0
+            self.y_val = 0
+            self.prev = None
+            self.loop = asyncio.get_event_loop()
+            self.go_for_time = 100
 
-    ep_chassis = ep_robot.chassis
-    ep_camera = ep_robot.camera
+        def move(self, json):
+            x_val, y_val = jsonToMovement(json)
+            self.ep_chassis.drive_speed(x=x_val, y=y_val, z=0, timeout=json['distance']/5)
+            self.ep_chassis.drive_speed(x=0, y=0, z=0, timeout=0)
 
-    x_val = 0.5
+        def camera(self):
+            img = self.ep_camera.read_cv2_image()
 
-    go_for_time = 100
+            results = model(img, stream=True)
 
-    ep_camera.start_video_stream(display=False)
-    for i in range(0, go_for_time):
-        if i < go_for_time/2:
-            ep_chassis.drive_speed(x=x_val, y=0, z=0, timeout=2)
-        else:
-            ep_chassis.drive_speed(x=-x_val, y=0, z=0, timeout=2)
-        camera()
+            for r in results:
+                boxes = r.boxes
+                for box in boxes:
+                    x1, y1, x2, y2 = box.xyxy[0]
+                    x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                    print(x1, y1, x2, y2)
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                    conf = math.ceil((box.conf[0] * 100)) / 100
+                    cls = int(box.cls[0])
+                    class_name = classNames[cls]
+                    label = f'{class_name}{conf}'
+                    t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+                    c2 = x1 + t_size[0], y1 - t_size[1] - 3
+                    cv2.rectangle(img, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)  # filled
+                    cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+            out.write(img)
+            cv2.imshow("Robot", img)
+            cv2.waitKey(1)
 
-    ep_chassis.drive_speed(x=0, y=0, z=0, timeout=0)
 
-    for i in range(0, 50):
-        camera()
+# Camera code
+# def camera():
+#     img = ep_camera.read_cv2_image()
+#
+#     results = model(img, stream=True)
+#
+#     for r in results:
+#         boxes = r.boxes
+#         for box in boxes:
+#             x1, y1, x2, y2 = box.xyxy[0]
+#             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+#             print(x1, y1, x2, y2)
+#             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+#             conf = math.ceil((box.conf[0] * 100)) / 100
+#             cls = int(box.cls[0])
+#             class_name = classNames[cls]
+#             label = f'{class_name}{conf}'
+#             t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+#             c2 = x1 + t_size[0], y1 - t_size[1] - 3
+#             cv2.rectangle(img, (x1, y1), c2, [255, 0, 255], -1, cv2.LINE_AA)  # filled
+#             cv2.putText(img, label, (x1, y1 - 2), 0, 1, [255, 255, 255], thickness=1, lineType=cv2.LINE_AA)
+#     out.write(img)
+#     cv2.imshow("Robot", img)
+#     cv2.waitKey(1)
 
-    cv2.destroyAllWindows()
-    ep_camera.stop_video_stream()
-
-    ep_robot.close()
-    out.release()
+# Robot movement code
+    # ep_robot = robot.Robot()
+    # ep_robot.initialize(conn_type="ap")
+    #
+    # ep_chassis = ep_robot.chassis
+    # ep_camera = ep_robot.camera
+    #
+    # x_val = 0
+    # y_val = 0
+    # prev = None
+    # loop = asyncio.get_event_loop()
+    #
+    # go_for_time = 100
+    #
+    # ep_camera.start_video_stream(display=False)
+    # for i in range(0, go_for_time):
+    #     if i % 3 == 0:
+    #         response = decision.response_dict
+    #         print(response)
+    #         if prev != response:
+    #             x_val, y_val = jsonToMovement(response)
+    #         else:
+    #             x_val, y_val = loop.run_until_complete(waitOneSecond())
+    #             loop.close()
+    #
+    #         if i < go_for_time/2:
+    #             ep_chassis.drive_speed(x=x_val, y=x_val, z=0, timeout=2)
+    #         else:
+    #             ep_chassis.drive_speed(x=-x_val, y=-x_val, z=0, timeout=2)
+    #         camera()
+    #
+    # ep_chassis.drive_speed(x=0, y=0, z=0, timeout=0)
+    #
+    # for i in range(0, go_for_time*2):
+    #     camera()
+    #
+    # cv2.destroyAllWindows()
+    # ep_camera.stop_video_stream()
+    #
+    # ep_robot.close()
+    # out.release()
