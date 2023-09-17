4e43aa9f1b54141d1ebafab12c76a396ed67f0fa
Xiao215 <xiaozhang20030215@gmail.com>
2023-09-17 07:11:24 -0400

    integrated cohere

--- communication/config-sample.py
+++ communication/config-sample.py
@@ -1,19 +1,21 @@
-# rename this file to config.py and fill in the values below
-OPENAI_API_KEY = "key"
+OPENAI_API_KEY = ""
+COHERE_API_KEY = ""
 
 TRANSCRIPT_FILE = "communication/transcriptions/transcript.txt"
 
-COMMAND_WORD = "whisper"
+COMMAND_WORD = "siri"
 
 PROMPT_INSTRUCTIONS = "You are trying to direct a blind man to walk. " \
     "Return the status of the decision below. " \
-    "If the person specify the word 'front' or similar, give a '0' as degree. Right is '90', left is '270', and feel free to give other angle from 0 to 360 if you think it fits the description of the user." \
+    "If the person specify the word 'front' or similar, give a '0' as degree. Right is '90' degree, back is '180', left is '270'," \
+    " and feel free to give other angle from 0 to 360 if you think it fits the description of the user." \
+    "For example, if the user says front-left or any similar command, you can give '315' degree." \
     "Also record down the distance in meter." \
-    "Return the result in JSON format:"
+    "Return the result in correct JSON format:"
 
 PROMPT_OUTPUT_FORMAT = """
 {
     "direction": "",
-    "distance": "",
+    "distance": ""
 }
 """
